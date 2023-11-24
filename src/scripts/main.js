1851e46d4cee9c1868a085b61ce11e53dbb7e4eb
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-11-24 12:41:39 +0200

    feat: add js functional for toggle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleBtn = document.getElementById('toggle-btn');
+  const cards = document.querySelectorAll('.card');
+
+  function updatePrices() {
+    const isMonthly = toggleBtn.checked;
+
+    cards.forEach(card => {
+      const priceElement = card.querySelector('.card__price');
+      const monthlyPrice = priceElement.getAttribute('data-monthly');
+      const yearlyPrice = priceElement.getAttribute('data-yearly');
+
+      priceElement.innerText = isMonthly ? monthlyPrice : yearlyPrice;
+    });
+  }
+
+  toggleBtn.addEventListener('change', updatePrices);
+
+  updatePrices();
+});
